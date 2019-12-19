0f6959a8d34d0bf957e739620127cb77b7414c51
Katakuari <katakuarii@gmail.com>
2019-12-19 12:06:23 +0100

    Create LockscreenCacheCopy_V2.bat
    New lockscreen copy with size filtering

---
+++ LockscreenCacheCopy_V2.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+SETLOCAL
+::Set maximum size of files to 280KB (for filtering)
+SET maxsize=280000
+
+::Create directory on desktop and change location to it
+MKDIR "%userprofile%\Desktop\lscache"
+CD "%userprofile%\Desktop\lscache"
+
+::Copy all files from cache to new directory on desktop
+COPY "%userprofile%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\*.*" .
+
+::Delete files below 280KB
+FOR /F "tokens=*" %%G IN ('DIR . /B') DO (
+IF %%~zG LSS %maxsize% del %%G
+)
+
+::Rename all filtered files to *.png (can be changed to .jpg)
+REN *.* *.png
