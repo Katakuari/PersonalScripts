d04ecaf0a4507ae0e94c6601880218cec5ff55e3
Katakuari <katakuarii@gmail.com>
2020-06-24 15:35:57 +0200

    Updated HB scripts

--- HBChoiceGameList.js
+++ HBChoiceGameList.js
@@ -1,9 +1,15 @@
-// Copy and paste this code into the browser's dev console
-// INTENDED FOR HUMBLE BUNDLE's CHOICE PAGE FOR SUBSCRIBERS
+// Copy and paste this code into the browsers dev console
+// INTENDED FOR HUMBLE BUNDLES CHOICE PAGE FOR SUBSCRIBERS
 
-var x = document.getElementsByClassName("content-choice-title").length; // Get number of games on page
-for (let i=0; i<x; i++){
-    let gamename = document.getElementsByClassName("content-choice-title")[i].innerText; // Fetch the name of the game
-    var gamelist = gamelist+"\n"+gamename; // Add the name at the end of the list
+
+const humbleChoiceGames = () => {
+    let numOfGames = document.getElementsByClassName('content-choice-title').length; // Get number of games on page
+    let gamelist; // Initialize gamelist variable for later use
+    for (let i = 0; i < numOfGames; i++){
+        let gamename = document.getElementsByClassName('content-choice-title')[i].innerText; // Fetch the name of the game
+        gamelist += '\n' + gamename; // Add name at the end of the list
+    };
+    console.log(gamelist); // Show the generated list
 };
-gamelist // Show the generated list
+
+humbleChoiceGames(); // Call function
--- HBGameKeyList.js
+++ HBGameKeyList.js
@@ -1,10 +1,15 @@
 // Copy and paste this code into the browser's dev console
-// INTENDED FOR HUMBLE BUNDLE's KEY PAGE IN YOUR ACCOUNT
+// INTENDED FOR HUMBLE BUNDLES KEY PAGE IN YOUR ACCOUNT
 // TO GENERATE A LIST OF UNUSED KEYS, PLEASE TICK THE BOX ON THE PAGE TO HIDE USED KEYS
 
-var x = document.getElementsByClassName("game-name").length; // Get number of items on page
-for (let i=1; i<x; i++){
-    let gamename = document.getElementsByClassName("game-name")[i].firstElementChild.innerText; // Fetch game name
-    var gamelist = gamelist+"\n"+gamename; // Add name at the end of the list
+const humbleKeyList = () => {
+    let numOfGames = document.getElementsByClassName('game-name').length; // Get number of items on page
+    let gamelist; // Initialize gamelist variable for later use
+    for (let i = 1; i < numOfGames; i++){
+        let gamename = document.getElementsByClassName('game-name')[i].firstElementChild.innerText; // Fetch game name
+        gamelist += '\n' + gamename; // Add name at the end of the list
+    };
+    console.log(gamelist); // Show the generated list
 };
-gamelist // Show the generated list
+
+humbleKeyList(); // Call function
