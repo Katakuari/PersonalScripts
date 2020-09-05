518d8030cedc1ab50bfa4b3b12023361f5b6481f
Katakuari <katakuarii@gmail.com>
2020-09-05 13:05:11 +0200

    Moved requirement check into its own function

--- ytdl-win-scripts/ytdl.ps1
+++ ytdl-win-scripts/ytdl.ps1
@@ -1,14 +1,13 @@
 # 05.09.2020
 
 New-Variable -Name parentdir -Value (Get-Location -PSProvider FileSystem) -Option ReadOnly # Get parent directory of script
-New-Variable -Name WC -Value (New-Object System.Net.WebClient) -Option ReadOnly
+New-Variable -Name WC -Value (New-Object System.Net.WebClient) -Option ReadOnly # Create WebClient and set links in case ffmpeg and/or ytdl are not found
 New-Variable -Name ffmpeglink -Value "https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-4.3-win64-static.zip" -Option ReadOnly
 New-Variable -Name ytdllink -Value "https://youtube-dl.org/downloads/latest/youtube-dl.exe" -Option ReadOnly
-$outdir = "$HOME\Downloads" # Destination for download
+$outdir = "$HOME\Downloads" # Destination for video download
 
-Write-Host "[ INFO ] Please make sure you have your txt configs, the youtube-dl.exe and the FFMPEG directory in the same directory as this script." -ForegroundColor Cyan
+Write-Host "[ INFO ] Please make sure you have your txt configs in the same directory as this script." -ForegroundColor Cyan
 Write-Host "[ INFO ] Download destination: $outdir" -ForegroundColor Cyan
-Write-host $parentdir
 
 function ytdl ([string]$fileformat) {
     # Get file format from selection
@@ -27,67 +26,26 @@ function ytdl ([string]$fileformat) {
         }
     }
 
-
     $vidlink = Read-Host -Prompt "Video or playlist link"
-    if ($null -eq (Get-ChildItem -File -Filter "youtube-dl.exe")) {
-        Write-Host "[ WARNING ] youtube-dl.exe not found!" -ForegroundColor Yellow
-        $ytdlmiss = Read-Host "Do you wish to download it now? [Y/N]"
-        switch ($ytdlmiss) {
-            Y {
-                try {
-                    $WC.DownloadFile($ytdllink, "$parentdir\youtube-dl.exe")
-                    Write-Host "[ INFO ] Done! Continuing..." -ForegroundColor Cyan
-                    Write-Host ""
-                }
-                catch {
-                    Write-Host "[ ERROR ] Could not download file! Check link in script!" -ForegroundColor Red
-                    Pause
-                    Exit
-                }
-            }
-            N { Exit }
-            Default { Exit }
-        }
-        
-    }
-
     Set-Location $outdir
 
+    if ($null -eq (Get-ChildItem -Path $parentdir -File -Filter "youtube-dl.exe")) {
+        # Check for ytdl, and download if missing
+        reqCheck("ytdlcheck")
+    }
+
     try {
-        # Try running FFMPEG from PATH var, and if found, use it 
+        # Try running FFMPEG from PATH, and if found, use it 
         ffmpeg -hide_banner -loglevel panic
         & $parentdir\youtube-dl.exe --config-location $parentdir\$config $vidlink
     }
     catch {
-        # If FFMPEG not working from PATH, check if FFMPEG\bin exists in parent directory
+        # If FFMPEG not working from PATH, check if FFMPEG\ exists in parent directory
         if ($null -eq (Get-ChildItem -Path $parentdir -Directory -Filter *ffmpeg*)) {
-            # If FFMPEG directory does not exist, exit
-            Write-Host "[ WARNING ] No FFMPEG folder found!" -ForegroundColor Yellow
-            $ffmpegmiss = Read-Host "Do you wish to download FFMPEG now? [Y/N]"
-            switch ($ffmpegmiss) {
-                Y {
-                    try {
-                        $WC.DownloadFile($ffmpeglink, "$parentdir\ffmpeg.zip")
-                        Expand-Archive -Path "$parentdir\ffmpeg.zip" -DestinationPath "$parentdir"
-                        Remove-Item -Path "$parentdir\ffmpeg.zip" -Force
-                        $ffmpegdir = Get-ChildItem -Path $parentdir -Directory -Filter *ffmpeg*
-                        Write-Host $ffmpegdir
-                        Write-Host "[ INFO ] Done! Continuing..." -ForegroundColor Cyan
-                        Write-Host ""
-                    }                 
-                    catch {
-                        Write-Host "[ ERROR ] Could not download file or set ffmpeg var! Check link in script and parent folder!" -ForegroundColor Red
-                        Pause
-                        Exit
-                    } 
-                }
-                N { Exit }
-                Default { Exit }
-            }
+            reqCheck("ffmpegcheck")
         }
-        else {
-            $ffmpegdir = Get-ChildItem -Path $parentdir -Directory -Filter *ffmpeg*
-        }
+        $ffmpegdir = Get-ChildItem -Path $parentdir -Directory -Filter *ffmpeg*
+
         & $parentdir\youtube-dl.exe --ffmpeg-location $parentdir\$ffmpegdir\bin --config-location $parentdir\$config $vidlink
     }
 
@@ -99,6 +57,56 @@ function ytdl ([string]$fileformat) {
     }
 }
 
+function reqCheck ([string]$req) {
+    switch ($req) {
+        ytdlcheck {
+            Write-Host "[ WARNING ] youtube-dl.exe not found!" -ForegroundColor Yellow
+            $ytdlmiss = Read-Host "Do you wish to download it now? [Y/N]"
+            switch ($ytdlmiss) {
+                Y {
+                    try {
+                        $WC.DownloadFile($ytdllink, "$parentdir\youtube-dl.exe")
+                        Write-Host "[ INFO ] Done! Continuing..." -ForegroundColor Cyan
+                        Write-Host ""
+                        break
+                    }
+                    catch {
+                        Write-Host "[ ERROR ] Could not download file! Check link in script!" -ForegroundColor Red
+                        Pause
+                        Exit
+                    }
+                }
+                N { Exit }
+                Default { Exit }
+            }
+        }
+
+        ffmpegcheck {
+            Write-Host "[ WARNING ] No FFMPEG folder found!" -ForegroundColor Yellow
+            $ffmpegmiss = Read-Host "Do you wish to download FFMPEG now? [Y/N]"
+            switch ($ffmpegmiss) {
+                Y {
+                    try {
+                        $WC.DownloadFile($ffmpeglink, "$parentdir\ffmpeg.zip")
+                        Expand-Archive -Path "$parentdir\ffmpeg.zip" -DestinationPath "$parentdir"
+                        Remove-Item -Path "$parentdir\ffmpeg.zip" -Force
+                        Write-Host "[ INFO ] Done! Continuing..." -ForegroundColor Cyan
+                        Write-Host ""
+                        break
+                    }                 
+                    catch {
+                        Write-Host "[ ERROR ] Could not download or extract file! Check link in script and parent folder!" -ForegroundColor Red
+                        Pause
+                        Exit
+                    } 
+                }
+                N { Exit }
+                Default { Exit }
+            }
+        }
+        Default { break }
+    }
+}
 
 function selection {
     Write-Host "Available download formats: 1 - mp4 | 2 - m4a | 3 - mp3 | 4 - custom"
@@ -117,6 +125,7 @@ function selection {
         Default { Exit }
     }
 }
+
 selection
 
 explorer.exe "$outdir"
