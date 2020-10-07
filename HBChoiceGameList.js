0656522a82cb12f415dcd38db94ab5f78fd37606
Katakuari <katakuarii@gmail.com>
2020-10-07 14:06:05 +0200

    Converted functions

--- HBChoiceGameList.js
+++ HBChoiceGameList.js
@@ -3,14 +3,14 @@
 // ONLY TESTED IN FIREFOX
 
 
-const humbleChoiceGames = () => {
+function humbleChoiceGames() {
     let numOfGames = document.getElementsByClassName('content-choice-title').length; // Get number of games on page
     let gamelist; // Initialize gamelist variable for later use
-    for (let i = 0; i < numOfGames; i++){
+    for (let i = 0; i < numOfGames; i++) {
         let gamename = document.getElementsByClassName('content-choice-title')[i].innerText; // Fetch the name of the game
         gamelist += '\n' + gamename; // Add name at the end of the list
     };
     console.log(gamelist); // Show the generated list
-};
+}
 
 humbleChoiceGames(); // Call function
--- HBUnredeemedGameList.js
+++ HBUnredeemedGameList.js
@@ -3,19 +3,19 @@
 // ONLY TESTED IN FIREFOX
 
 
-const humbleUnredeemedGameList = () => {
+function humbleUnredeemedGameList() {
     let numOfGames = document.getElementsByClassName('game-name').length; // Get number of games on page
     let gamelist; // Initialize gamelist var for later use
     let gamename; // Initialize gamename var for later use
-    for (let i = 1; i < numOfGames; i++){
+    for (let i = 1; i < numOfGames; i++) {
         gamename = document.getElementsByClassName('game-name')[i].firstElementChild.innerText; // Fetch current game name
         gamelist += '\n' + gamename; // Add current game at the end of the list
     };
     console.log(gamelist); // Log the generated list in the console
-};
+}
 
 if (document.getElementById('hide-redeemed').checked === false){ // Check if checkbox has already been clicked
     document.getElementById('hide-redeemed').click(); // Click checkbox to hide redeemed keys
 };
 
-setTimeout(humbleUnredeemedGameList, 2000); // Call function with a 2 second delay
+setTimeout(humbleUnredeemedGameList(), 2000); // Call function with a 2 second delay
